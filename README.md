258c206f549c07ca042870a873d0ad53ca1b11a8
nanrto2 <hameem.quader@ontariotechu.net>
2023-11-16 16:03:28 -0500

    Version change
    Version 3.0

--- README.md
+++
@@ -1,4 +0,0 @@
-# nanrto2.github.io
-Hameem Quader 100786057
-
-Web & Script Programming - INFR-3120U
---
+++ server/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'  
+  });
+});
+
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'About Me'  
+  });
+});
+
+
+/* GET Products page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { 
+    title: 'My Projects'  
+  });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { 
+    title: 'Services'  
+  });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { 
+    title: 'Contact Me'  
+  });
+});
+
+module.exports = router;
